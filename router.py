80759ca00a67f87af87c2db90d4ae6dafa1f248d
Dustin Thurston <dthurston1996@gmail.com>
2017-11-13 21:24:30 -0500

    add checksum attempt

--- router.py
+++ router.py
@@ -177,6 +177,23 @@ class myRouter:
                 errorPacket =  ethHeader + icmpErrorHeader
                 socket.sendto(errorPacket, sendPlace)
 
+	"""
+	calculates checkSum
+	obtained from:
+	https://stackoverflow.com/questions/1767910/checksum-udp-calculation-python
+	"""
+	def carry_around_add(self, a, b):
+		c = a + b
+		return (c & 0xffff) + (c >> 16)
+
+	def calcChecksum(self, msg):
+   		s = 0
+   		for i in range(0, len(msg), 2):
+       			w = ord(msg[i]) + (ord(msg[i+1]) << 8)
+       			s = self.carry_around_add(s, w)
+   		return str(~s & 0xffff)
+
+		
         """
 	Runs the router
 	"""
